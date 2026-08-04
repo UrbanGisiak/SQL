13ce4d505b6fdee5a27184a8cf61365aebf5e8c8
Łukasz <mackiewicz.lukasz.97@gmail.com>
2026-08-05 01:15:50 +0200

    Create SELECT WHERE.sql

---
+++ PYTANIA_SQL/SELECT WHERE.sql
@@ -0,0 +1,12 @@
+-- Podstawowe zapytanie SQL z klauzulą WHERE
+-- można użyć do filtrowania danych w tabeli na podstawie określonych kryteriów, np. wartości w kolumnach.
+-- WHERE jest używane do określenia warunków, które muszą być spełnione, aby wiersze zostały zwrócone w wyniku zapytania.
+-- PRZYKŁAD: UŻYTEK_GRUNTOWY = 'rolniczy' -- wybieramy tylko te wiersze, gdzie kolumna "UŻYTEK_GRUNTOWY" ma wartość 'rolniczy'
+
+SELECT * FROM -- wybierz z ...
+
+"przykladowa_tabela" -- wybieramy z tabeli "wstaw swoją nazwę tabeli"
+
+WHERE -- zapytanie warunkowe
+
+"xyz" = 'aaa' -- warunek, który sprawdza, czy kolumna "xyz" ma wartość 'aaa'
